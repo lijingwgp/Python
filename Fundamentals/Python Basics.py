ee1ef7a5fcca19376425f380e4dd69fc12eda9f9
Jing <lijingwgp@yahoo.com>
2017-12-25 12:48:19 -0800

    Update Python Basics.py

--- Fundamentals/Python Basics.py
+++ Fundamentals/Python Basics.py
@@ -86,8 +86,19 @@ europe['italy'] = 'rome'
 europe['germany'] = 'berlin'
 # Remove australia
 del(europe['australia']
-
-
+# Dictionary of dictionaries
+europe = { 'spain': { 'capital':'madrid', 'population':46.77 },
+           'france': { 'capital':'paris', 'population':66.03 },
+           'germany': { 'capital':'berlin', 'population':80.62 },
+           'norway': { 'capital':'oslo', 'population':5.084 } }
+# Print out the capital of France
+print(europe['france']['capital'])
+# Create sub-dictionary data
+data = {'capital':'rome','population':59.83}
+# Add data to europe under key 'italy'
+europe['italy'] = data
+    
+    
 ## functions
 def SquareIt(x):
     return x * x

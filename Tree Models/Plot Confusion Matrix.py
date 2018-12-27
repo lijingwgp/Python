1cdc59e3ade3da9029f89046651ce74779e81c52
Jing <lijingwgp@yahoo.com>
2018-12-27 11:45:15 -0500

    Add files via upload

---
+++ Tree Models/Plot Confusion Matrix.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 27 11:42:35 2018
+
+@author: jing.o.li
+"""
+
+# Using the following code we can plot the confusion matrix for any of the 
+# tree-based models.
+
+def plot_confusion_matrix(cm, classes, normalize=False):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    
+    cmap = plt.cm.Blues
+    title = "Confusion Matrix"
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        cm = np.around(cm, decimals=3)
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, cm[i, j],
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+
+# Now, we can take a look at the confusion matrix of out first model:
+y_pred = treeClassifier.predict(x_test)
+cfm = confusion_matrix(y_test, y_pred, labels=[0, 1])
+plt.figure(figsize=(10,6))
+plot_confusion_matrix(cfm, classes=["<=50K", ">50K"], normalize=True)

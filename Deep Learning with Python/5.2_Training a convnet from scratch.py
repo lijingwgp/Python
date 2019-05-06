e3abd193fe220c13eca9bbcec2e73ef73fdf802e
Jing <lijingwgp@yahoo.com>
2019-05-06 15:11:58 -0400

    Update 5.2_Training a convnet from scratch.py

--- Deep Learning with Python/5.2_Training a convnet from scratch.py
+++ Deep Learning with Python/5.2_Training a convnet from scratch.py
@@ -110,6 +110,7 @@ print('total test dog images:', len(os.listdir(test_dogs_dir)))
 #test_cats_dir = r"C:\Users\jing.o.li\Desktop\test\cats"
 #test_dogs_dir = r"C:\Users\jing.o.li\Desktop\test\dogs"
 
+# build a convnet
 model = models.Sequential()
 model.add(layers.Conv2D(32, (3, 3), activation='relu',input_shape=(150, 150, 3)))
 model.add(layers.MaxPooling2D((2, 2)))
@@ -124,6 +125,7 @@ model.add(layers.Dense(512, activation='relu'))
 model.add(layers.Dense(1, activation='sigmoid'))
 model.summary()
 
+# configure the model for training
 model.compile(loss='binary_crossentropy',optimizer=optimizers.RMSprop(lr=1e-4),
               metrics=['acc'])
 

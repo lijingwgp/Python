f8f8e2eea81cfad7ac3723eb00f9a94ea466242b
Jing <lijingwgp@yahoo.com>
2019-05-06 15:14:55 -0400

    Update 5.2_Training a convnet from scratch.py

--- Deep Learning with Python/5.2_Training a convnet from scratch.py
+++ Deep Learning with Python/5.2_Training a convnet from scratch.py
@@ -142,7 +142,6 @@ train_generator = train_datagen.flow_from_directory(train_dir,
                                                     target_size=(150, 150),
                                                     batch_size=20,
                                                     class_mode='binary')
-
 valid_generator = test_datagen.flow_from_directory(valid_dir,
                                                    target_size=(150, 150),
                                                    batch_size=20,

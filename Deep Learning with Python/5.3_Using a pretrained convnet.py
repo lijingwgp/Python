d18bd7d34476b7cc371037021256e8b508f31f4b
Jing <lijingwgp@yahoo.com>
2019-05-09 13:31:36 -0400

    Update 5.3_Using a pretrained convnet.py

--- Deep Learning with Python/5.3_Using a pretrained convnet.py
+++ Deep Learning with Python/5.3_Using a pretrained convnet.py
@@ -139,3 +139,33 @@ history = model.fit_generator(
   epochs = 30,
   validation_data = valid_generator,
   validation_steps = 50)
+
+
+
+#############################################
+##### Fine-tuning with a few top layers #####
+#############################################
+
+# freezing all layers up to a specific one
+conv_base.trainable = True
+set_trainable = False
+
+for layer in conv_base.layers:
+    if layer.name == 'block5_conv1':
+        set_trainable = True
+    if set_trainable:
+        layer.trainable = True
+    else:layer.trainable = False
+
+model.compile(loss='binary_crossentropy',optimizer=optimizers.RMSprop(lr=1e-5),
+              metrics=['acc'])
+
+history = model.fit_generator(train_generator,steps_per_epoch=100,epochs=100,
+                              validation_data=validation_generator,
+                              validation_steps=50)
+
+# access test set results
+test_generator = test_datagen.flow_from_directory(test_dir,target_size=(150, 150),
+                                                  batch_size=20,class_mode='binary')
+test_loss, test_acc = model.evaluate_generator(test_generator, steps=50)
+print('test acc:', test_acc)

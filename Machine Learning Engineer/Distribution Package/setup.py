73476de16396d4ce82811f3392c04dda8168d4ac
Jing <lijingwgp@yahoo.com>
2020-06-17 22:33:07 -0400

    Add files via upload

---
+++ Machine Learning Engineer/Distribution Package/setup.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 17 22:26:15 2020
+
+@author: 607991
+"""
+
+from setuptools import setup
+
+setup(name='distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions'],
+      zip_safe=False)
